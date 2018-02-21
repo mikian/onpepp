a733b9de9a57c83c739bb8c15a514e4fcc6136cd
Mikko Kokkonen <mikko@mikian.com>
2018-02-21 00:28:13 +0000

    Add syncing schedules

--- sync.rb
+++ sync.rb
@@ -24,8 +24,35 @@ class EasyPepp
   end
 
   def users
-    response = self.class.get('/users.json', format: :plain)
-    JSON.parse response, symbolize_names: true
+    @users ||= begin
+      response = self.class.get('/users.json', format: :plain)
+      JSON.parse response, symbolize_names: true
+    end
+  end
+
+  def shifts
+    @shifts ||= begin
+      response = self.class.get('/shifts.json', format: :plain)
+      JSON.parse response, symbolize_names: true
+    end
+  end
+end
+
+class OnFleet
+  include HTTParty
+  logger ::Logger.new(STDOUT), :debug, :curl
+
+  base_uri 'https://onfleet.com/api/v2'
+  basic_auth ENV.fetch('ONFLEET_APIKEY'), ''
+  headers 'Content-Type' => 'application/json', 'Accept' => 'application/json'
+
+  def update_schedule(worker_id, schedule)
+    self.class.put(
+      "/workers/#{worker_id}/schedule",
+      body: schedule.to_json,
+      # format: :json,
+      headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    )
   end
 end
 
@@ -48,4 +75,26 @@ easypepp.users.each do |user|
   else
     Onfleet::Worker.create(params)
   end
+end
+
+# Sync schedules
+schedule = easypepp.shifts.each_with_object(Hash.new {|h, k| h[k] = []}) do |shift, schedule|
+  shift[:assigned_user_ids].each do |uid|
+    driver = easypepp.users.find { |u| u[:id] == uid }
+    worker = Onfleet::Worker.list.find { |w| w.name == "#{driver[:first_name]} #{driver[:last_name]}" }
+
+    starts_at = Time.parse(shift[:starts_at])
+    ends_at = Time.parse(shift[:ends_at])
+
+    schedule[worker.id] << {
+      date: starts_at.strftime('%Y-%m-%d'),
+      shifts: [[starts_at.strftime('%s').to_i * 1000, ends_at.strftime('%s').to_i * 1000]],
+      timezone: 'Europe/London'
+    }
+  end
+
+  schedule
+end
+schedule.each do |worker, entries|
+  OnFleet.new.update_schedule(worker, entries: entries)
 end
